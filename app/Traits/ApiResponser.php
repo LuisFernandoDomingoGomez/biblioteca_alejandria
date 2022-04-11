9a00a8214d33dbe3d5158797a0940cd64b1ad2f4
Luis Fernando Domingo Gomez <al222010066@gmail.com>
2022-04-11 12:19:49 -0500

    Arreglos final

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -7,34 +7,42 @@ use Illuminate\Database\Eloquent\Model;
  *  retornar respuestas satisfactorias en Formato Json */
 trait apiResponser
 {
-    private function successResponse($data, $code)
-    {
-        return  response()->json($data,$code);  
-    }
+    protected function successResponse($data, $code)
+  {
+    return  response()->json($data,$code);  
+  }
 
-    /** Metodo para retornar una respuesta de error que 
-     * tendra el mensaje y el codigo */
-    protected function errorResponse($message, $code)
-    {
-        return response()->json(['error'=>$message,'code'=>$code],$code);
-    }
-    
-    /** Mostrara la lista completas de usuarios  si la respuesta
-     * no tiene un codigo por defecto mandamos un 200*/
-    protected function showAll(Collection $collection, $code = 200)
-    {
-        return $this->successResponse(['data'=> $collection], $code);
-    }
+  /** Metodo para retornar una respuesta de error que 
+   * tendra el mensaje y el codigo */
+  protected function errorResponse($message, $code)
+  {
+    return response()->json(['error'=>$message,'code'=>$code],$code);
+  }
 
-    /* Mensaje de actualizar desde la api */
-    protected function showUpdate($message, $code = 200){
-        return $this->successResponse(['data'=> $message], $code);
-    }
+/** Mostrara la lista completas de usuarios  si la r
+ * espuesta no tiene un codigo por defecto mandamos un 200*/
+  protected function showAll(Collection $collection, $code = 200){
+    return $this->successResponse($collection, $code);
+  }
 
-    /** Mostrara solo un registro y este resive una instancia */
-    protected function showOne(Model $instance, $code = 200)
-    {
-        return $this->successResponse(['data'=> $instance], $code);
-    }
+  /** Mostrara solo un registro y este resive una instancia */
+  protected function showOne(Model $instance, $code = 200){
+    return $this->successResponse($instance, $code);
+  }
+
+  /* Mensaje de guardar desde la api */
+  protected function showMessage($message, $code = 200){
+    return $this->successResponse(['data'=> $message], $code);
+  }
+
+  /* Mensaje de actualizar desde la api */
+  protected function showUpdate($message, $code = 200){
+    return $this->successResponse(['data'=> $message], $code);
+  }
+
+  /* Mensaje de eliminar desde la api */
+  protected function showDelete($message, $code = 200){
+    return $this->successResponse(['data'=> $message], $code);
+  }
 
 }
